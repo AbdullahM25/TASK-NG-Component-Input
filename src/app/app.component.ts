6b67e9992020b12693931f196ecff8c72954d2db
AbdullahM25 <AbdullahSMishari@gmail.com>
2025-04-06 23:26:11 +0300

    done w challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { pets } from '../data/pets';
+import { PetComponent } from './pet/pet.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PetComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TASK-NG-Passing-Data';
